38a568b59906b6296073379c9bb88c3204bec476
ducphucle <leducphuc1994@gmail.com>
2017-09-19 10:10:19 +0700

    build

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.fetchApi = this.fetchApi.bind(this);
     this.setEmail = this.setEmail.bind(this);
     this.state = {
-      phase: 1,
+      phase: 0,
       email: '',
       loaded: true,
       error: '',
